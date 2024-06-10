0efa5553424a27c5117146a29d9c353e8928d1b5
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-06-10 11:39:42 +0100

    validation

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -4,9 +4,14 @@ part "counter_state.dart";
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState()) {
-    on<CounterIncrement>(
-        (event, emit) => emit(state.copyWith(count: state.count + 1)));
-    on<CounterDecrement>(
-        (event, emit) => emit(state.copyWith(count: state.count - 1)));
+    on<CounterIncrement>((event, emit) {
+      emit(state.copyWith(count: state.count + 1));
+    });
+    on<CounterDecrement>((event, emit) {
+      if (state.count == 0) {
+        return;
+      }
+      emit(state.copyWith(count: state.count - 1));
+    });
   }
 }
